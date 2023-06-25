97f66093392ed050817016ecfba9681eba338a5c
Wiz <wizbarbosa@gmail.com>
2023-06-25 01:07:30 -0500

    focusing-alright

--- background.js
+++ background.js
@@ -1,9 +1,22 @@
 chrome.commands.onCommand.addListener(function (command) {
   if (command === "searchRepos") {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.update(tabs[0].id, {
-        url: "https://github.com/capitanbarbosa?tab=repositories&q=",
-      });
+      chrome.tabs.create(
+        {
+          url: "https://github.com/capitanbarbosa?tab=repositories&q=",
+        },
+        function (newTab) {
+          // Listen for the tab to be fully loaded
+          chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
+            if (info.status === "complete" && tabId === newTab.id) {
+              chrome.tabs.sendMessage(newTab.id, {
+                message: "focus_element",
+              });
+              chrome.tabs.onUpdated.removeListener(listener); // remove the listener now that we've found our tab
+            }
+          });
+        }
+      );
     });
   }
 });
